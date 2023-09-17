7e72565f512f7313124606608445791982bbe049
itedya <bdomanski@protonmail.com>
2023-09-17 12:33:14 +0200

    Add basic global error catcher

--- public/index.php
+++ public/index.php
@@ -1,1 +1,14 @@
-Hello world! You are now viewing: <?= $_SERVER['REQUEST_URI']; ?>
+<?php
+
+include_once "./database/connection.php";
+
+function __start()
+{
+}
+
+try {
+    __start();
+} catch (Exception $e) {
+    http_response_code(500);
+    echo "Internal server error";
+}
