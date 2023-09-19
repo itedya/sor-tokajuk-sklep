9d5e706a676c2a68bc3b041099a3e4b5911fd9fc
itedya <bdomanski@protonmail.com>
2023-09-19 19:53:59 +0200

    Third login page version

--- auth/login.php
+++ auth/login.php
@@ -8,21 +8,21 @@
     <title>Logowanie</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body class="bg-neutral-900 min-h-screen w-full flex justify-center items-center p-4">
-<form method="POST" action="/api/auth/login.php" class="w-full max-w-xl p-4 flex flex-col gap-8 bg-gradient-to-tr from-neutral-900 to-neutral-800 rounded-xl">
+<body class="bg-neutral-900 min-h-screen w-full flex justify-center items-center p-4 bg-gradient-to-tr from-neutral-900 to-neutral-800">
+<form method="POST" action="/api/auth/login.php" class="w-full max-w-xl p-4 flex flex-col gap-8 rounded-xl">
     <h1 class="text-4xl font-bold text-center text-neutral-300">Logowanie</h1>
 
     <div class="flex flex-col gap-4">
         <div class="flex flex-col gap-1">
             <label for="email" class="text-lg text-neutral-300 font-semibold mx-2">Email</label>
             <input type="email" name="email" id="email"
-                   class="p-4 bg-neutral-800 rounded-xl border-neutral-700 focus:outline-none text-lg text-neutral-300"/>
+                   class="p-4 bg-neutral-800 rounded-xl border-4 border-transparent outline-none focus:outline-none text-lg text-neutral-300 focus:border-neutral-700 duration-300"/>
         </div>
 
         <div class="flex flex-col gap-1">
             <label for="password" class="text-lg text-neutral-300 font-semibold mx-2">Hasło</label>
             <input type="password" name="password" id="password"
-                   class="p-4 bg-neutral-800 rounded-xl border-neutral-700 focus:outline-none text-lg text-neutral-300"/>
+                   class="p-4 bg-neutral-800 rounded-xl border-4 border-transparent outline-none focus:outline-none text-lg text-neutral-300 focus:border-neutral-700 duration-300"/>
         </div>
     </div>
 
