e7a1fb911e1577d4425f99b2cc3b7ba256e77fff
itedya <bdomanski@protonmail.com>
2023-09-24 22:30:38 +0200

    Reformat the register.php file

--- auth/register.php
+++ auth/register.php
@@ -5,7 +5,7 @@ loadFrontendTooling("..");
 
 $errors = array();
 
-function postMethod() 
+function postMethod()
 {
 
     if (!isset($_POST['email'])) ValidationErrorFacade::add('email', 'Email jest wymagany!');
@@ -15,58 +15,57 @@ function postMethod()
     register($_POST['email'], $_POST['password']);
 }
 
-function register($email, $password, $repeat_password) 
+function register($email, $password, $repeat_password)
 {
     $passwordHash = password_hash($password, PASSWORD_DEFAULT);
 
-    if (empty($email) OR empty($password) OR empty($passwordRepeat)) {
-        array_push($errors,"All fields are required");
+    if (empty($email) or empty($password) or empty($passwordRepeat)) {
+        array_push($errors, "All fields are required");
     }
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
         array_push($errors, "Email is not correct");
     }
-    if (strlen($password)<8) {
+    if (strlen($password) < 8) {
         array_push($errors, "Password must be at least 8 characters long");
     }
-    if ($password!==$repeat_password) {
+    if ($password !== $repeat_password) {
         array_push($errors, "Password does not match");
     }
 
-        $conn = require "../database.php";
+    $conn = require "../database.php";
 
-        $sql = "SELECT * FROM users WHERE email = '$email'";
-        $result = mysqli_query($conn, $sql);
-        $rowCount = mysqli_num_rows($result);
-            if ($rowCount>0) {
-                array_push($errors, "User with this email already exists");
-            }
-            var_dump($errors);
-            if (count($errors)>0) {
-                foreach ($errors as $error) {
-                    echo $error;
-                }
+    $sql = "SELECT * FROM users WHERE email = '$email'";
+    $result = mysqli_query($conn, $sql);
+    $rowCount = mysqli_num_rows($result);
+    if ($rowCount > 0) {
+        array_push($errors, "User with this email already exists");
+    }
+    var_dump($errors);
+    if (count($errors) > 0) {
+        foreach ($errors as $error) {
+            echo $error;
+        }
 
-            }else{
-                
-                $sql = "INSERT INTO users(email, password) VALUES (?, ?)";
-                $stmt = mysqli_stmt_init($conn);
-                $prepareStmt = mysqli_stmt_prepare($stmt, $sql);
-                if ($prepareStmt) {
-                    mysqli_stmt_bind_param($stmt,"ss", $email, $passwordHash);
-                    mysqli_stmt_execute($stmt);
-                    echo "Registered successfully";
-                }else{
-                    die("Error");
-                }
-            }
-        
-        session_start();
+    } else {
 
-        $_SESSION['user_id'] = $id;
-        header('Location: ../index.php');
+        $sql = "INSERT INTO users(email, password) VALUES (?, ?)";
+        $stmt = mysqli_stmt_init($conn);
+        $prepareStmt = mysqli_stmt_prepare($stmt, $sql);
+        if ($prepareStmt) {
+            mysqli_stmt_bind_param($stmt, "ss", $email, $passwordHash);
+            mysqli_stmt_execute($stmt);
+            echo "Registered successfully";
+        } else {
+            die("Error");
+        }
+    }
+
+    session_start();
+
+    $_SESSION['user_id'] = $id;
+    header('Location: ../index.php');
 
 }
-
 
 
 $body = <<<HTML
