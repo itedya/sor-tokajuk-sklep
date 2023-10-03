d93ed04850370af5671eff7dbb690e3a7ac05e9b
itedya <bdomanski@protonmail.com>
2023-10-03 22:05:36 +0200

    Reformat database.sql file

--- database.sql
+++ database.sql
@@ -1,21 +1,20 @@
 CREATE TABLE `users`
 (
-    `id`              int          NOT NULL AUTO_INCREMENT,
-    `email`           varchar(255) NOT NULL,
-    `is_verified`     boolean NOT NULL DEFAULT FALSE,
-    `password`           varchar(255) NOT NULL,
-    `created_at` datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `id`          int          NOT NULL AUTO_INCREMENT,
+    `email`       varchar(255) NOT NULL,
+    `is_verified` boolean      NOT NULL DEFAULT FALSE,
+    `password`    varchar(255) NOT NULL,
+    `created_at`  datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT pk_users_id PRIMARY KEY (`id`),
     CONSTRAINT uq_users_email UNIQUE (`email`)
 );
-CREATE TABLE `email_verification_attempts` (
-  `id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `hash` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `email_verification_attempts`
+(
+    `id`      int          NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `user_id` int          NOT NULL,
+    `hash`    varchar(255) NOT NULL,
 
-ALTER TABLE `email_verification_attempts`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `uq_email_verification_attempts_hash` (`hash`),
-  ADD KEY `fk_email_verification_attempts_user_id` (`user_id`);
+    CONSTRAINT UNIQUE KEY `uq_email_verification_attempts_hash` (`hash`),
+    CONSTRAINT FOREIGN KEY `fk_email_verification_attempts_user_id` (`user_id`) REFERENCES `users` (`id`)
+);
