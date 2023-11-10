bbc14ac2d8fc7c9093626768dcc09378c3a9f0c5
itedya <bdomanski@protonmail.com>
2023-11-10 21:11:49 +0100

    Fix detected errors by manual testing in login page

--- auth/login.php
+++ auth/login.php
@@ -15,20 +15,37 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     old_input_add("email", $email);
 
+    // validate email
+    $emailError = validate_email($email);
+    if ($emailError !== null) validation_errors_add("email", $emailError);
+
+    // validate password
+    $passwordError = validate_password($password);
+    if ($passwordError !== null) validation_errors_add("password", $passwordError);
+
+    if (!validation_errors_is_empty()) redirect_and_kill("login.php");
+
     $db = get_db_connection();
-    $data = db_query_row($db, "SELECT id, password FROM users WHERE email = ?", [$email]);
+    $data = db_query_row($db, "SELECT id, is_verified, password FROM users WHERE email = ?", [$email]);
 
     if ($data === null) {
         validation_errors_add("email", "Dane do logowanie nie zgadzają się.");
         redirect_and_kill("login.php");
     }
 
+    if ($data['is_verified'] === 0) {
+        validation_errors_add("email", "Konto nie zostało jeszcze zweryfikowane.");
+        redirect_and_kill("login.php");
+    }
+
     if (!password_verify($password, $data['password'])) {
-        validation_errors_add('email', 'Dane do logowania nie zgadzają się');
+        validation_errors_add('email', 'Dane do logowania nie zgadzają się.');
         redirect_and_kill("login.php");
     }
 
     auth_login($data['id']);
+
+    redirect_and_kill(config("app.url") . "/");
 }
 
 echo render_in_layout(function () {
@@ -38,7 +55,7 @@ echo render_in_layout(function () {
             <h1 class="text-4xl font-bold text-center text-neutral-300">Logowanie</h1>
 
             <div class="flex flex-col gap-4">
-                <?= render_textfield(label: 'Email', name: 'email', type: 'email') ?>
+                <?= render_textfield(label: 'Email', name: 'email', type: 'text') ?>
                 <?= render_textfield(label: 'Hasło', name: 'password', type: 'password') ?>
             </div>
 
