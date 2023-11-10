80f2a382370ba284188f959da16eb96e58a2b3d5
itedya <bdomanski@protonmail.com>
2023-11-11 00:04:30 +0100

    Changed frontend in confirm email (manual testing fixes)

--- auth/confirm-email.php
+++ auth/confirm-email.php
@@ -37,8 +37,10 @@ if (get_query_param('hash') !== null) {
 }
 
 echo render_in_layout(function () { ?>
-    <div class="flex flex-col justify-center items-center p-4 gap-3">
-        <h1 class="text-3xl text-zinc-300">Twój email został zweryfikowany</h1>
-        <a class="text-xl text-blue-200" href="../index.php">Powrót do strony głównej</a>
+    <div class="flex flex-col justify-center items-center p-4 gap-8">
+        <h1 class="text-4xl font-bold text-center text-neutral-300">Sukces!</h1>
+        <p class="text-xl text-neutral-200">Twój email został zweryfikowany, teraz możesz się zalogować</p>
+        <a class="px-8 py-2 bg-blue-600 text-neutral-200 font-semibold rounded-lg disabled:bg-blue-400 duration-300"
+           href="<?= config("app.url") . "/auth/login.php" ?>">Powrót do strony logowania</a>
     </div>
 <?php });
