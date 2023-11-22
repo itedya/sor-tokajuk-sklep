e071d20051e7cda1485cbf7a02599893279321f6
itedya <bdomanski@protonmail.com>
2023-11-22 18:25:05 +0100

    Fix table component rendering

--- tooling/components/render_table.php
+++ tooling/components/render_table.php
@@ -1,6 +1,6 @@
 <?php
 
-function render_table(array $columns): string
+function render_table(array $columns, array $rows): string
 {
     $columnClasses = join(' ', ['border', 'border-neutral-700', 'bg-neutral-800', 'text-left', 'font-normal', 'p-2', 'whitespace-nowrap']);
     $valueClasses = join(' ', ['border', 'border-neutral-700', 'bg-neutral-900', 'p-2', 'text-neutral-400']);
@@ -8,19 +8,22 @@ function render_table(array $columns): string
     ob_start(); ?>
     <div class="overflow-x-auto w-full">
         <table class="border-collapse border border-neutral-700 bg-neutral-800 w-full">
-            <tbody>
-            <?php foreach ($columns as $column): ?>
+            <?php if (count($columns) !== 0): ?>
+                <thead>
                 <tr>
-                    <?php foreach ($column as $row): ?>
-                        <?php if ($row['type'] === "COLUMN"): ?>
-                            <th class="<?= $columnClasses ?>">
-                                <?= ($row['is_html'] ?? false) ? $row['value'] : htmlspecialchars($row['value']) ?>
-                            </th>
-                        <?php else: ?>
-                            <td class="<?= $valueClasses ?>">
-                                <?= ($row['is_html'] ?? false) ? $row['value'] : htmlspecialchars($row['value']) ?>
-                            </td>
-                        <?php endif; ?>
+                    <?php foreach ($columns as $column): ?>
+                        <th class="<?= $columnClasses ?>">
+                            <?= $column ?>
+                        </th>
+                    <?php endforeach; ?>
+                </tr>
+                </thead>
+            <?php endif; ?>
+            <tbody>
+            <?php foreach ($rows as $row): ?>
+                <tr>
+                    <?php foreach ($row as $field): ?>
+                        <td class="<?= $valueClasses ?>"><?= $field['value'] ?></td>
                     <?php endforeach; ?>
                 </tr>
             <?php endforeach; ?>
