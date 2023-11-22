50f3b90c5f9871f14ffd07d2cacde26ad3c0660d
Ferboss9 <ferboss150@gmail.com>
2023-11-22 08:31:53 +0100

    add backend for edit user page

--- management/users/edit.php
+++ management/users/edit.php
@@ -6,13 +6,44 @@ gate_redirect_if_unauthorized();
 gate_redirect_if_not_an_admin();
 
 if ($_SERVER['REQUEST_METHOD'] === "POST") {
-    // ...
+    $id = $_POST['id'] ?? null;
+    $email = $_POST['email'] ?? null;
+    $is_admin = $_POST['is_admin'] ?? null;
+
+    if ($id === null) validation_errors_add("id", "Id nie może być puste.");
+    if ($email === null) validation_errors_add("email", "Email nie może być pusty.");
+    if ($is_admin === null) validation_errors_add("is_admin", "Rola nie może być pusta.");
+
+    if (!validation_errors_is_empty()) {
+        redirect_and_kill(base_url("/management/users/edit.php", ['id' => $id]));
+    }
+
+    $is_admin = intval($is_admin);
+    if ($is_admin !== 0 && $is_admin !== 1) {
+        validation_errors_add("is_admin", "Pole rola posiada niepoprawną wartość.");
+        redirect_and_kill(base_url("/management/users/edit.php", ['id' => $id]));
+    }
+
+    db_transaction(function (mysqli $db) use ($id, $email, $is_admin) {
+        $user = db_query_row($db, "SELECT * FROM users WHERE id = ?", [$id]);
+        if ($user === null) redirect_and_kill(base_url("/management/users.php"));
+
+        db_execute_stmt($db, "UPDATE users SET email = ?, is_admin = ? WHERE id = ?", [$email, $is_admin, $id]);
+    });
+
+    session_flash("user_edited", true);
+    redirect_and_kill(base_url("/management/users/edit.php", ['id' => $id]));
 }
 
 $id = $_GET['id'] ?? null;
 if ($id === null) redirect_and_kill(base_url("/management/users.php"));
 
-$users = ['id' => $id];
+$users = db_query_row(get_db_connection(), "SELECT * FROM users WHERE id = ?", [$id]);
+if ($users === null) redirect_and_kill(base_url("/management/users.php"));
+
+if (!old_input_has("email")) old_input_add("email", $users['email']);
+if (!old_input_has("is_admin")) old_input_add("is_admin", $users['is_admin']);
+else old_input_add("is_admin", intval(old_input_get('is_admin')));
 
 echo render_in_layout(function () use ($users) { ?>
     <div class="container mx-auto p-4 gap-8 flex flex-col">
