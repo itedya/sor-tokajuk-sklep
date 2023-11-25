6624eb6de912af94a57baaa18b57443c7ffd2a8d
Ferboss9 <ferboss150@gmail.com>
2023-11-25 18:37:14 +0100

    backend create payment type

--- management/payment-types/create.php
+++ management/payment-types/create.php
@@ -5,9 +5,51 @@ require_once __DIR__ . '/../../tooling/autoload.php';
 gate_redirect_if_unauthorized();
 gate_redirect_if_not_an_admin();
 
+if (session_get('after_payment_type_creation', false)) {
+    echo render_in_layout(function () { ?>
+        <div class="container mx-auto">
+            <div class="flex flex-col gap-4 justify-center items-center p-4">
+                <h2 class="text-3xl font-bold text-neutral-300 text-center">Sukces</h2>
+                <p class="text-neutral-200 text-center">Pomyślnie dodano nowy sposób płatności</p>
+                <a href="<?= base_url('/management/payment-types.php') ?>" class="px-8 py-2 bg-blue-600 text-neutral-200 rounded-xl font-bold">Powrót do listy sposobów płatności</a>
+            </div>
+        </div>
+    <?php });
+    return;
+}
+
+if ($_SERVER['REQUEST_METHOD'] === "POST") {
+    if (empty($_POST['name'])) validation_errors_add("name", "Nazwa jest wymagana.");
+
+    if (!validation_errors_is_empty()) redirect_and_kill($_SERVER['REQUEST_URI']);
+
+    if (gettype($_POST['name']) !== "string") validation_errors_add("name", "Nazwa musi być tekstem.");
+
+    if (!validation_errors_is_empty()) redirect_and_kill($_SERVER['REQUEST_URI']);
+
+    $name = $_POST['name'];
+
+    if (strlen($name) < 3) validation_errors_add("name", "Nazwa musi mieć więcej niż 3 znaki");
+    if (strlen($name) > 64) validation_errors_add("name", "Nazwa nie może mieć więcej niż 64 znaki.");
+
+    if (!validation_errors_is_empty()) redirect_and_kill($_SERVER['REQUEST_URI']);
+    
+    db_transaction(function(mysqli $db) use ($name) {
+        if (database_payment_types_does_exist_by_name($db, $name)) {
+            validation_errors_add("name", "Metoda płatności o tej nazwie już istnieje!");
+            redirect_and_kill($_SERVER['REQUEST_URI']);       
+        }
+
+        database_payment_types_create($db, $name);
+    });
+
+    session_flash('after_payment_type_creation', true);
+    redirect_and_kill($_SERVER['REQUEST_URI']);
+}
+
 echo render_in_layout(function() { ?>
 <div class="container mx-auto">
-    <form class="flex flex-col gap-4 max-w-3xl mx-auto">
+    <form action="<?= $_SERVER['REQUEST_URI'] ?>" method="POST" class="flex flex-col gap-4 max-w-3xl mx-auto">
         <h2 class="text-3xl font-bold text-neutral-300 text-center">Stwórz metodę płatności</h2>
 
         <?= render_textfield(label: "Nazwa", name: "name") ?>
--- tooling/helper_functions/database_payment_types.php
+++ tooling/helper_functions/database_payment_types.php
@@ -1,5 +1,15 @@
 <?php
 
+function database_payment_types_create(mysqli $db, string $name): void
+{
+    db_execute_stmt($db, "INSERT INTO payment_types (name) VALUES (?)", [$name]);
+}
+
+function database_payment_types_does_exist_by_name(mysqli $db, string $name): bool
+{
+    return db_query_row($db, "SELECT id FROM payment_types WHERE deleted_at IS NULL AND name = ?", [$name]) !== null;
+}
+
 function database_payment_types_get(mysqli $db): array
 {
     return db_query_rows($db, "SELECT * FROM payment_types WHERE deleted_at IS NULL", []);
