ec2aeef97d2fee048e2611baee8e2a447242dee3
itedya <bdomanski@protonmail.com>
2023-11-25 15:54:03 +0100

    Add orders title

--- client-panel/orders.php
+++ client-panel/orders.php
@@ -9,25 +9,26 @@ $db = get_db_connection();
 $orders = database_orders_get_by_user_id($db, auth_get_user_id());
 
 ob_start(); ?>
+<div class="flex flex-col gap-4 justify-center">
+    <h2 class="text-3xl font-bold text-center">Zamówienia</h2>
+    <?= render_table(
+        ["Identyfikator", "Status", "Akcje"],
+        array_map(function ($row) {
+            $statusHtml = match ($row['status']) {
+                0 => '<span class="text-neutral-400">W trakcie realizacji</span>',
+                1 => '<span class="text-green-400">Zrealizowane</span>',
+                default => '<span class="text-neutral-400">Nieznany status</span>'
+            };
 
-<?= render_table(
-    ["Identyfikator", "Status", "Akcje"],
-    array_map(function ($row) {
-        $statusHtml = match ($row['status']) {
-            0 => '<span class="text-neutral-400">W trakcie realizacji</span>',
-            1 => '<span class="text-green-400">Zrealizowane</span>',
-            default => '<span class="text-neutral-400">Nieznany status</span>'
-        };
-
-        return [
-            ['value' => "#" . $row['id']],
-            ['value' => $statusHtml],
-            ['value' => '<a href="' . base_url("/orders/details.php", [
-                    'id' => $row['id']
-                ]) . '" class="px-4 py-2 bg-neutral-600 text-neutral-200 font-semibold rounded-lg text-center">Szczegóły</a>', 'is_html' => true]
-        ];
-    }, $orders)
-) ?>
-
+            return [
+                ['value' => "#" . $row['id']],
+                ['value' => $statusHtml],
+                ['value' => '<a href="' . base_url("/orders/details.php", [
+                        'id' => $row['id']
+                    ]) . '" class="px-4 py-2 bg-neutral-600 text-neutral-200 font-semibold rounded-lg text-center">Szczegóły</a>', 'is_html' => true]
+            ];
+        }, $orders)
+    ) ?>
+</div>
 <?php
 echo ob_get_clean();
