39aa31163e763cd54b1ff4e680211180928e6249
itedya <bdomanski@protonmail.com>
2023-11-26 15:17:54 +0100

    Add management of delivery methods frontend

---
+++ management/delivery-methods.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once __DIR__ . '/../tooling/autoload.php';
+
+gate_redirect_if_unauthorized();
+gate_redirect_if_not_an_admin();
+
+$rows = [
+    [
+        'id' => 1,
+        'name' => 'Pierwszy sposób',
+        'price' => 20.00,
+    ],
+    [
+        'id' => 2,
+        'name' => 'Drugi sposób',
+        'price' => 30.00,
+    ],
+];
+
+echo render_in_layout(function() use ($rows) { ?>
+    <div class="container mx-auto">
+        <div class="flex flex-col max-w-3xl mx-auto gap-8">
+            <h2 class="text-3xl font-bold text-neutral-300 text-center">Zarządzanie sposobami dostawy</h2>
+            <?= render_table(['Nazwa', 'Cena', '', ''], array_map(function ($row) {
+                $deleteUrl = base_url('/management/delivery-methods/delete.php', ['id' => $row['id']]);
+                $editUrl = base_url('/management/delivery-methods/edit.php', ['id' => $row['id']]);
+
+                return [
+                    ['value' => $row['name']],
+                    ['value' => $row['price']],
+                    ['value' => sprintf('<a href="%s" class="px-8 py-2 bg-red-600 text-neutral-200 rounded-xl font-bold">Usuń</a>', $deleteUrl), 'is_html' => true],
+                    ['value' => sprintf('<a href="%s" class="px-8 py-2 bg-yellow-600 text-neutral-200 rounded-xl font-bold">Edytuj</a>', $editUrl), 'is_html' => true],
+                ];
+            }, $rows)) ?>
+        </div>
+    </div>
+<?php });
