25d4b255afc131efdd7c52a029aff1ef82adb27d
itedya <bdomanski@protonmail.com>
2023-11-28 15:29:58 +0100

    add products to home page

--- index.php
+++ index.php
@@ -2,10 +2,29 @@
 
 require_once './tooling/autoload.php';
 
-$userId = auth_get_user_id();
+$db = get_db_connection();
+$products = database_products_get($db);
+$db->close();
 
-echo render_in_layout(function () use ($userId) { ?>
-    <div class="text-3xl text-center text-neutral-300 p-4">
-        Strona główna <?= $userId ?>
+echo render_in_layout(function () use ($products) { ?>
+    <style type="text/tailwindcss">
+        <?= file_get_contents(__DIR__ . '/../../assets/css/product-card.css') ?>
+    </style>
+
+    <div class="container mx-auto">
+        <div class="grid grid-cols-1 auto-grid-rows gap-8 xl:grid-cols-2">
+            <?php foreach ($products as $product): ?>
+                <div class="item"
+                     onclick="window.location.href = `<?= base_url('product.php', ['id' => $product['id']]) ?>`">
+                    <div class="item-img"
+                         style="background-image: url('<?= base_url("/images/" . $product['image']) ?>');">
+                    </div>
+
+                    <h2 class="item-title"><?= htmlspecialchars($product['name']) ?></h2>
+                    <p class="item-description"><?= htmlspecialchars($product['description']) ?></p>
+                    <span class="item-price"><?= htmlspecialchars($product['price']) ?>zł</span>
+                </div>
+            <?php endforeach; ?>
+        </div>
     </div>
 <?php });
--- tooling/helper_functions/database_products.php
+++ tooling/helper_functions/database_products.php
@@ -11,3 +11,23 @@ SQL;
 
     return db_query_rows($db, $query, [$orderId]);
 }
+
+function database_products_get(mysqli $db): array
+{
+    $query = <<<SQL
+SELECT products.*, pi.image FROM products
+LEFT JOIN (
+    SELECT id, product_id, image
+    FROM products_images pi1
+    WHERE id = (
+        SELECT id
+        FROM products_images pi2
+        WHERE pi1.product_id = pi2.product_id
+        ORDER BY pi2.id
+        LIMIT 1
+    )
+) pi ON pi.product_id = products.id
+SQL;
+
+    return db_query_rows($db, $query, []);
+}
